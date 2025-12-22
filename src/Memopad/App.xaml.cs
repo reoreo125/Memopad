a62934a1c07a7fc5e75ffaceb8c648485e1c8ac4
reoreo125 <reoreo125@gmail.com>
2025-12-22 16:39:28 +0900

    R3 WPFプロバイダ初期化とPrism.Wpf導入
    アプリ起動時にR3のWPF用プロバイダを初期化し、未処理例外をデバッグ出力するよう対応。不必要なusingを整理し、System.DiagnosticsとR3関連のusingを追加。さらに、Prism.WpfおよびR3Extensions.WPFのNuGetパッケージ参照をプロジェクトに追加。

--- src/Memopad/App.xaml.cs
+++ src/Memopad/App.xaml.cs
@@ -1,9 +1,13 @@
-using System.Configuration;
-using System.Data;
+using System.Diagnostics;
 using System.Windows;
+using R3;
 
 namespace Reoreo125.Memopad;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        WpfProviderInitializer.SetDefaultObservableSystem(ex => Trace.WriteLine($"R3 UnhandledException:{ex}"));
+    }
 }
