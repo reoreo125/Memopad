e6ec77662d65d43794ce49784f9293a1aeb5da13
reoreo125 <reoreo125@gmail.com>
2025-12-24 12:47:34 +0900

    MainWindowViewModelの最適化

--- src/Memopad/ViewModels/Windows/MainWindowViewModel.cs
+++ src/Memopad/ViewModels/Windows/MainWindowViewModel.cs
@@ -1,3 +1,4 @@
+using System.Data.Common;
 using System.Windows;
 using Prism.Mvvm;
 using R3;
@@ -7,69 +8,47 @@ namespace Reoreo125.Memopad.ViewModels.Windows;
 
 public class MainWindowViewModel : BindableBase, IDisposable
 {
-    public BindableReactiveProperty<string> Title { get; } = new BindableReactiveProperty<string>();
-    public BindableReactiveProperty<string> Text { get; } = new BindableReactiveProperty<string>();
-    public BindableReactiveProperty<string> FileName { get; } = new BindableReactiveProperty<string>("新規テキスト");
-    public BindableReactiveProperty<int> Row { get; } = new BindableReactiveProperty<int>();
-    public BindableReactiveProperty<int> Column { get; } = new BindableReactiveProperty<int>();
-    private DisposableBag _disposableCollection = new();
+    public BindableReactiveProperty<string> Title { get; }
+    public BindableReactiveProperty<string> Text { get; }
+    public BindableReactiveProperty<int> Row { get; }
+    public BindableReactiveProperty<int> Column { get; }
 
     protected IMemopadCoreService MemopadCoreService => _memopadCoreService;
     private readonly IMemopadCoreService _memopadCoreService;
+    private DisposableBag _disposableCollection = new();
 
     public MainWindowViewModel(IMemopadCoreService memopadCoreService)
     {
         _memopadCoreService = memopadCoreService ?? throw new ArgumentNullException(nameof(memopadCoreService));
 
-        Title.Value = _memopadCoreService.Title;
+        #region Model -> ViewModel -> View
+        // 保存されていない変更がある状態になった時（＊）やタイトル変更
+        Title = Observable.Merge
+            (
+                MemopadCoreService.DirtyChanged.Select(_ => MemopadCoreService.Title),
+                MemopadCoreService.TitleChanged
+            )
+            .ToBindableReactiveProperty(MemopadCoreService.Title);
+        // テキストが変更された時
+        Text = MemopadCoreService.TextChanged
+            .Where(value => Text!.Value != value) // 循環防止
+            .ToBindableReactiveProperty(string.Empty);
+        Row = new BindableReactiveProperty<int>(1);
+        Column = new BindableReactiveProperty<int>(1);
+        #endregion
 
         #region View -> ViewModel -> Model
         // テキスト変更 
-        Text.Debounce(TimeSpan.FromMilliseconds(0))
-            .Subscribe(text =>
-            {
-                if (text is null) return;
-
-                MemopadCoreService.ChangeText(text);
-            })
+        Text.Where(value => value is not null)
+            .Debounce(TimeSpan.FromMilliseconds(500))
+            .Subscribe(value => MemopadCoreService.ChangeText(value))
             .AddTo(ref _disposableCollection);
-        Row.Subscribe(row =>
-            {
-                if (row < 1) return;
-                MemopadCoreService.ChangeSelection(row, MemopadCoreService.Column);
-            })
+        Row.Where(value => 0 < value)
+            .Subscribe(value => MemopadCoreService.ChangeSelection(value, MemopadCoreService.Column))
             .AddTo(ref _disposableCollection);
-        Column.Subscribe(column =>
-            {
-                if (column < 1) return;
-                MemopadCoreService.ChangeSelection(MemopadCoreService.Row, column);
-            })
-            .AddTo(ref _disposableCollection);
-        #endregion
-
-        #region Model -> ViewModel -> View
-        // テキストが変更された時
-        MemopadCoreService.TextChanged.Subscribe(text =>
-            {
-                // テキストボックスがトリガーになっていない場合、つまりモデル側からテキストボックスが更新される場合
-                if (Text.Value != text)
-                {
-                    Text.Value = text;
-                    return;
-                }
-            })
-            .AddTo(ref _disposableCollection);
-        // 保存されていない変更がある状態になった時
-        MemopadCoreService.DirtyChanged.Subscribe(_ =>
-            {
-                Title.Value = MemopadCoreService.Title;
-            })
-            .AddTo(ref _disposableCollection);
-        MemopadCoreService.TitleChanged.Subscribe(_ =>
-            {
-                Title.Value = MemopadCoreService.Title;
-            })
-            .AddTo(ref _disposableCollection);
+        Column.Where(value => 0 < value)
+              .Subscribe(value => MemopadCoreService.ChangeSelection(MemopadCoreService.Row, value))
+              .AddTo(ref _disposableCollection);
         #endregion
     }
 
