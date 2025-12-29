267dfae4f72531185cccc76cf9a298e6a1806ae5
reoreo125 <reoreo125@gmail.com>
2025-12-29 18:20:59 +0900

    FontDialogのサイズ周りの実装

--- src/ViewModels/Dialogs/FontDialogViewModel.cs
+++ src/ViewModels/Dialogs/FontDialogViewModel.cs
@@ -1,3 +1,6 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using R3;
 using Reoreo125.Memopad.Models;
 using Reoreo125.Memopad.Models.Commands;
@@ -16,6 +19,12 @@ namespace Reoreo125.Memopad.ViewModels.Dialogs
             RequestClose.Invoke(new DialogResult(ButtonResult.OK));
         });
 
+        public BindableReactiveProperty<string> Size { get; }
+        public BindableReactiveProperty<string?> ListBoxSize { get; }
+        public string[] PresetSizes { get; } = new[]
+        { "8", "9", "10", "11", "12", "14", "16", "18", "20", "22", "24", "26", "28", "36", "48", "72" };
+
+
         public IEditorService EditorService => _editorService;
         private readonly IEditorService _editorService;
         protected ISettingsService SettingsService => _settingsService;
@@ -28,13 +37,22 @@ namespace Reoreo125.Memopad.ViewModels.Dialogs
             _editorService = editorService ?? throw new ArgumentNullException(nameof(editorService));
             _settingsService = settingsService ?? throw new ArgumentNullException(nameof(SettingsService));
 
-            #region Model -> ViewModel -> View
+            Size = new(SettingsService.Settings.FontSize.Value.ToString());
+            ListBoxSize = new(SettingsService.Settings.FontSize.Value.ToString());
+            ListBoxSize
+                .Where(x => x != null)
+                .Subscribe(x => Size.Value = x!)
+                .AddTo(ref _disposableCollection);
+            Size
+                .Subscribe(x => {
+                    if (string.IsNullOrEmpty(x)) return;
 
-            #endregion
+                    if (int.TryParse(x, out int val) && val <= 0) Size.Value = "1";
 
-            #region View -> ViewModel -> Model
-
-            #endregion
+                    if (PresetSizes.Contains(x)) ListBoxSize.Value = x;
+                    else ListBoxSize.Value = null; // リストにない数値なら選択解除
+                })
+                .AddTo(ref _disposableCollection);
         }
         public void OnDialogOpened(IDialogParameters parameters)
         {
--- src/Views/Dialogs/FontDialog.xaml.cs
+++ src/Views/Dialogs/FontDialog.xaml.cs
@@ -9,6 +9,36 @@ namespace Reoreo125.Memopad.Views.Dialogs
         public FontDialog()
         {
             InitializeComponent();
+
+            SetupPasteHandler(SizeTextBox);
+        }
+        private void NumberOnly_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // 正規表現で数字(0-9)以外を弾く
+            e.Handled = !System.Text.RegularExpressions.Regex.IsMatch(e.Text, "^[0-9]+$");
+        }
+
+        private void NumberOnly_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // スペースキーを禁止
+            if (e.Key == Key.Space) e.Handled = true;
+        }
+        // 貼り付け（Ctrl+V）対策：コンストラクタなどで追加
+        private void SetupPasteHandler(TextBox textBox)
+        {
+            DataObject.AddPastingHandler(textBox, (s, e) =>
+            {
+                if (e.DataObject.GetDataPresent(typeof(string)))
+                {
+                    var text = (string)e.DataObject.GetData(typeof(string));
+                    // 数字以外が含まれる文字列の貼り付けをキャンセル
+                    if (!System.Text.RegularExpressions.Regex.IsMatch(text, "^[0-9]+$"))
+                    {
+                        e.CancelCommand();
+                    }
+                }
+                else { e.CancelCommand(); }
+            });
         }
     }
 }
