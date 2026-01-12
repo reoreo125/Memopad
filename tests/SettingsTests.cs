a903e10bbcb34c11154abf0aef3945543c69af84
reoreo125 <reoreo125@gmail.com>
2026-01-13 01:50:32 +0900

    FontFamilyNameに対するテストの実装

--- tests/SettingsTests.cs
+++ tests/SettingsTests.cs
@@ -4,6 +4,7 @@ namespace Reoreo125.Memopad.Tests
 {
     public class SettingsTests
     {
+        #region LastOpenedFolderPath
         [Theory(DisplayName = "【正常系】LastOpenedFolderPath:最後に開いたフォルダパスが有効な場合、値が維持されること")]
         [InlineData(@"c:\")]
         [InlineData(@"C:\Users\Public\Documents")]
@@ -35,5 +36,38 @@ namespace Reoreo125.Memopad.Tests
 
             Assert.Equal(settings.LastOpenedFolderPath.Value, Defaults.LastOpenedFolderPath);
         }
+        #endregion
+
+        #region FontFamilyName
+        [Theory(DisplayName = "【正常系】FontFamilyName:フォントファミリー名が有効な場合、値が維持されること")]
+        [InlineData("Arial")]
+        [InlineData("Times New Roman")]
+        [InlineData("MS Gothic")]
+        public void FontFamilyName_ValidValue_ShouldKeepValue(string validFontFamilyName)
+        {
+            var settings = new Settings();
+            settings.FontFamilyName.Value = validFontFamilyName;
+
+            var settingsService = new SettingsService();
+            settingsService.Validate(settings);
+
+            Assert.Equal(settings.FontFamilyName.Value, validFontFamilyName);
+        }
+
+        [Theory(DisplayName = "【異常系】FontFamilyName:フォントファミリー名が無効な場合、デフォルト値に復元されること")]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData("TestFontFamily")]
+        public void FontFamilyName_InvalidValue_ShouldFallbackToDefault(string invalidFontFamilyName)
+        {
+            var settings = new Settings();
+            settings.FontFamilyName.Value = invalidFontFamilyName;
+
+            var settingsService = new SettingsService();
+            settingsService.Validate(settings);
+
+            Assert.Equal(settings.FontFamilyName.Value, Defaults.FontFamilyName);
+        }
+        #endregion
     }
 }
