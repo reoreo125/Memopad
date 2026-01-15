c1d12f6fadacfc98395519cc611ea91b89821c43
reoreo125 <reoreo125@gmail.com>
2026-01-16 08:01:11 +0900

    DialogServiceに対するテストを少し実装

---
+++ tests/1_Unit/Models/DialogServiceTests.cs
@@ -0,0 +1,40 @@
+using NSubstitute;
+using Prism.Dialogs;
+using Reoreo125.Memopad.Models;
+using Reoreo125.Memopad.Views.Dialogs;
+using System;
+using IPrismDialogService = Prism.Dialogs.IDialogService;
+using DialogService = Reoreo125.Memopad.Models.DialogService;
+
+namespace Reoreo125.Memopad.Tests.Unit.Models;
+
+public class DialogServiceTests
+{
+    #region ConfirmSave
+    [Fact(DisplayName = "【正常系】ConfirmSave: 確認ダイアログが表示されること")]
+    public void ConfirmSave_ShouldShowDialog()
+    {
+        var prismDialogService = Substitute.For<IPrismDialogService>();
+        var settingsService = Substitute.For<ISettingsService>();
+
+        var expectedResult = new DialogResult(ButtonResult.OK);
+        prismDialogService.WhenForAnyArgs(x => x.ShowDialog(default, default, default))
+            .Do(callInfo =>
+            {
+                var callback = callInfo[2] as dynamic;
+                callback?.Invoke(expectedResult);
+            });
+
+        var dialogService = new DialogService
+        {
+            PrismDialogService = prismDialogService,
+            SettingsService = settingsService
+        };
+
+        var actualResult = dialogService.ConfirmSave("TestText");
+
+        Assert.NotNull(actualResult);
+        Assert.Equal(expectedResult, actualResult);
+    }
+    #endregion
+}
